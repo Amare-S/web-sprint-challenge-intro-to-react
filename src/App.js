5e074a8d85da4ff916dfb23887336cab34d455b6
Amare Shakur <amare-shakur@lambdastudents.com>
2021-02-19 19:44:56 -0500

    app.js is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,7 +10,18 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [starwars, setStarwarsApi] = useState("");
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/")
+    .then(res => setStarwarsApi(res.data.message))
+    .catch(err => console.log(err));
 
+  },[]);
+
+
+
+
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
